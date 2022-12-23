b2b0d64d111106899285ab749a028b0653023c47
Aman Singh Chauhan <amansingh9723chauhan@gmail.com>
2022-12-23 15:31:29 +0530

    updates

--- src/routes/service.routes.js
+++ src/routes/service.routes.js
@@ -29,7 +29,7 @@ app.get('/', async (req, res) => {
                               },
                           },
             },
-            { $match: { task: { $regex: query } } },
+            { $match: { task: { $regex: query, $options: 'i' } } },
             {
                 $match: filter ? { category: filter } : {},
             },
